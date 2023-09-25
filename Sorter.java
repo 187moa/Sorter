c26699d41656ad0cb23df3fd8010dcbe36cef221
Basel <bsbsv3@gmail.com>
2023-09-25 22:21:18 +0300

    added insertion sort

--- Sorter.java
+++ Sorter.java
@@ -1,5 +1,24 @@
+import java.util.Arrays;
+
 public class Sorter {
     public static void main(String[] args) {
         System.out.println("hala");
+        int[] data ={20, 12, 10, 15,2 };
+        insertionSort(data);
+        System.out.println(Arrays.toString(data));
+
+    }
+
+    public static void insertionSort(int[] array) {
+        int n = array.length;
+        for (int j = 1; j < n; j++) {
+            int key = array[j];
+            int i = j - 1;
+            while ((i > -1) && (array[i] > key)) {
+                array[i + 1] = array[i];
+                i--;
+            }
+            array[i + 1] = key;
+        }
     }
 }
