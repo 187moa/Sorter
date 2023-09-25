3184cfd9299dd8289f15977cdd2b812dabea7604
Basel <bsbsv3@gmail.com>
2023-09-25 22:38:41 +0300

    added three sorts

--- Sorter.java
+++ Sorter.java
@@ -8,6 +8,27 @@ public class Sorter {
         System.out.println(Arrays.toString(data));
 
     }
+    public static void selectionSort(int array[]) {
+        int size = array.length;
+
+        for (int step = 0; step < size - 1; step++) {
+            int min_idx = step;
+
+            for (int i = step + 1; i < size; i++) {
+
+                // To sort in descending order, change > to < in this line.
+                // Select the minimum element in each loop.
+                if (array[i] < array[min_idx]) {
+                    min_idx = i;
+                }
+            }
+
+            // put min at the correct position
+            int temp = array[step];
+            array[step] = array[min_idx];
+            array[min_idx] = temp;
+        }
+    }
 
     public static void insertionSort(int[] array) {
         int n = array.length;
@@ -20,5 +41,8 @@ public class Sorter {
             }
             array[i + 1] = key;
         }
+    }
+    public static void sort(int[] arr){
+        insertionSort(arr);
     }
 }
